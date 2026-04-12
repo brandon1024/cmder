856da0840d3df9a7422cbcf38441497c3a17d282
Brandon Richardson <brandon.richardson@siemens.com>
2026-04-12 08:28:04 +0200

    feat(getopt): introduce negated bool var type
    This revision introduces a new type NegatedBoolVar in package getopt for
    representing boolean flags which are the inverse of another flag. For
    instance, configuring a '--no-gpg-sign' flag.
    
    Additionally, the CounterVar example has been improved with a more
    motivating example.
    
    Update Go toolchain to 1.26.2.

--- getopt/bool.go
+++ getopt/bool.go
@@ -1,6 +1,47 @@
 package getopt
 
-import "flag"
+import (
+	"flag"
+	"strconv"
+)
+
+// NegatedBoolVar is a boolean [flag.Value] for negating other flag values. A typical use case for NegatedBoolVar is to
+// register flags which disable/unset other flags.
+//
+//	--show
+//	--no-show
+type NegatedBoolVar bool
+
+// NegatedBool builds a [NegatedBoolVar] backed by b.
+func NegatedBool(v *bool) *NegatedBoolVar {
+	return (*NegatedBoolVar)(v)
+}
+
+// String returns the string representation of the (negated) boolean value.
+func (b *NegatedBoolVar) String() string {
+	return strconv.FormatBool(!bool(*b))
+}
+
+// Set updates the value of the flag. The given value must be a string parseable by [strconv.ParseBool]. The flag value
+// is updated with the (negated) value s.
+func (b *NegatedBoolVar) Set(s string) error {
+	val, err := strconv.ParseBool(s)
+	if err == nil {
+		*b = NegatedBoolVar(!val)
+	}
+
+	return err
+}
+
+// IsBoolFlag marks the flag is not accepting args.
+func (b *NegatedBoolVar) IsBoolFlag() bool {
+	return true
+}
+
+// Get fulfills the [flag.Getter] interface, allowing typed access to the flag value. In this case, returns a bool.
+func (b *NegatedBoolVar) Get() any {
+	return !bool(*b)
+}
 
 // boolFlag is a [flag.Value] that also implements a method IsBoolFlag, used to determine if the flag accepts an
 // argument or not.
